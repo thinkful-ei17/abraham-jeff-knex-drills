2f0e78decdaee38e61e2395f33a15a29e3ead904
Chris Klanac <cklanac@gmail.com>
2017-04-26 08:43:49 -0400

    add naive and soph approaches

--- hydrate.js
+++ hydrate.js
@@ -13,7 +13,8 @@ knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeI
   .orderBy('date', 'asc')
   .limit(10)
   .then(results => {
-    
+
+    // naive approach
     const hydrated = {};
     results.forEach(row => {
       if (!(row.id in hydrated)) {
@@ -31,6 +32,29 @@ knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeI
       });
     });
     console.log(JSON.stringify(hydrated, null, 2));
+
+    // sophisticated approach
+    const raw_data = people;
+    const results = [], lookup = {};
+    for (let thing of raw_data) {
+      if (!lookup[thing.id]) {
+        lookup[thing.id] = {
+          id: thing.id,
+          name: thing.name,
+          age: thing.age,
+          pets: []
+        };
+        results.push(lookup[thing.id]);
+      }
+
+      lookup[thing.id].pets.push({
+        petName: thing.petName,
+        petType: thing.petType
+      });
+    }
+    console.log(JSON.stringify(results, null, 2))
+
+
   });
 
 
