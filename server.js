95261c357728cd10bba9bba62f3d080c467dc58a
Chris Klanac <cklanac@gmail.com>
2017-07-07 07:45:58 -0400

    remove body-parser

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const { DATABASE, PORT } = require('./config');
 const knex = require('knex')(DATABASE);
 
 const app = express();
-app.use(bodyParser.json());
 
 app.get('/restaurants', (req, res) => {
   knex.select('id', 'name', 'cuisine', 'borough')
