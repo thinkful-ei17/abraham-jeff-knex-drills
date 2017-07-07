bf5e3632ad5fdc4c7f809a970360b512044f78ce
Chris Klanac <cklanac@gmail.com>
2017-07-07 07:48:47 -0400

    Add comments

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const DATABASE_URL = process.env.DATABASE_URL
 exports.DATABASE = {
   client: 'pg',
   connection: DATABASE_URL,
-  pool: { min: 0, max: 3 },
-  debug: true
+  pool: { min: 0, max: 3 }, // Fix issue w/ ElephantSQL
+  debug: true               // Outputs knex debugging information
 };
 
 exports.PORT = process.env.PORT || 8080; 
