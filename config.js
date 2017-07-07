2848407799b6389b05f1b149c252ec5804312724
Chris Klanac <cklanac@gmail.com>
2017-07-07 07:38:49 -0400

    Clear starter

--- config.js
+++ config.js
@@ -1,31 +1,15 @@
+'use strict';
 
-
-// const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/todos-app';
-
-// postgres://USERNAME:PASSWORD@stampy.db.elephantsql.com:5432/DATABASE
+// For Local Database
+const DATABASE_URL = process.env.DATABASE_URL
+                   ||  global.DATABASE_URL 
+                   || 'postgresql://dev:dev@localhost/dev-restaurants-app';
 
 exports.DATABASE = {
   client: 'pg',
   connection: DATABASE_URL,
-  // debug: true
+  pool: { min: 0, max: 3 },
+  debug: true
 };
 
 exports.PORT = process.env.PORT || 8080; 
-
-
-
-// Require Knex and make connection
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    database: 'dev-restaurants-app'
-  },
-});
-
-// If you're using ElephantSQL then the connection will look like this
-/*
-const knex = require('knex')({
-  client: 'pg',
-  connection:'postgres://USERNAME:PASSWORD@stampy.db.elephantsql.com:5432/DATABASE'
-}
-*/
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const { DATABASE, PORT } = require('./config');
-
 const knex = require('knex')(DATABASE);
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/restaurants', (req, res) => {
-
   knex.select('id', 'name', 'cuisine', 'borough')
     .from('restaurants')
     .limit(10)
