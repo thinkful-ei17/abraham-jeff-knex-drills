6b6ba67fc14da9b2ae1e06d887c98bad8660ba79
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-03 10:12:25 -0700

    Added first 7 Drills

--- drills.js
+++ drills.js
@@ -7,12 +7,65 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
+
+// 1. Get all restaurants
+// knex 
+//   .select()
+//   .from('restaurants')
+//   .then(console.log);
+
+// 2. Get Italian restaurants
+// knex 
+//   .select()
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .then(console.log);
+
+// 3. Get 10 Italian restaurants, subset of fields
+// knex
+//   .select('id', 'name')
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .limit(10)
+//   .then(console.log);
+
+// 4. Count of Thai restaurants
+// knex 
+//   .count()
+//   .from('restaurants')
+//   .where('cuisine', 'Thai')
+//   .then(console.log);
+
+// 5. Count of restaurants
+// knex
+//   .count()
+//   .from('restaurants')
+//   .then(console.log);
+
+// 6. Count of Thai restaurants in zip code
+// knex 
+//   .count()
+//   .from('restaurants')
+//   .where('cuisine', 'Thai')
+//   .andWhere('address_zipcode', '11372')
+//   .then(console.log);
+
+// 7. Italian restaurants in one of several zip codes
+knex 
+  .select('id', 'name')
   .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+  .where('cuisine', 'Italian')
+  .whereIn('address_zipcode', ['10012', '10013', '10014'])
+  .limit(5)
+  .orderBy('name', 'asc')
+  .then(console.log);
 
 
 // Destroy the connection pool
